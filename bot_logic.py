ec533c86bc9acfba98e174e53c0c0f13f57141b7
.ww.ww. <144980603+yuenpcompany@users.noreply.github.com>
2026-05-26 21:30:39 +0500

    Update bot_logic.py
    обновлениеее

--- bot_logic.py
+++ bot_logic.py
@@ -1,19 +1,11 @@
 import random
 
-def gen_pass(pass_length):
-    elements = "+-/*!&$#?=@<>123456789"
-    password = ""
-    for i in range(pass_length):
-        password += random.choice(elements)
-    return password
-
-def gen_emodji():
-    emodji = ["😀", "😊", "😂", "🤣"]
-    return random.choice(emodji)
+def gen_pass(p):
+    e = "+-/*!&$#?=@<>123456789"
+    r = ""
+    for i in range(p):
+        r += random.choice(e)
+    return r
 
 def flip_coin():
-    flip = random.randint(0, 1)
-    if flip == 0:
-        return "ОРЕЛ"
-    else:
-        return "РЕШКА"
+    return random.choice(["орел", "решка"])
